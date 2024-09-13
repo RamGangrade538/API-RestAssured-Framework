1db3b8a7a3b3182be6c3da89934c4ee83784bd0e
Ram Gangrade <gangraderam538@gmail.com>
2024-09-13 19:10:03 +0530

    Added PostRequest

---
+++ src/test/java/postRequest.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+
+public class postRequest {
+@Test
+public void postRequest(){
+    Response resp  =RestAssured.
+            given()
+            .header("Content-Type", "application/json")
+            .body("{\n" +
+                    "    \"_id\":\"252d3bca-d9bb-476c-9a97-562d685e235c\",\n" +
+                    "    \"name\": \"Sri Lankan Airways\",\n" +
+                    "    \"country\": \"Sri Lanka\",\n" +
+                    "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
+                    "    \"slogan\": \"From Sri Lanka\",\n" +
+                    "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
+                    "    \"website\": \"www.srilankaaairways.com\",\n" +
+                    "    \"established\": \"1990\"\n" +
+                    "}")
+            .when()
+            .post("https://api.instantwebtools.net/v1/airlines")
+            .then()
+            .assertThat()
+            .statusCode(200)
+            .extract()
+            .response();
+    System.out.println(resp.prettyPrint());
+
+}
+}
